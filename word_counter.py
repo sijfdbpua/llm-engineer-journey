c608ebadf447fd6c2c5b7d5507873a47dc8cff4e
Bogdan Pavlenko <bogdan2008pavlenko@gmail.com>
2026-06-30 23:48:15 +0300

    import counter

--- word_counter.py
+++ word_counter.py
@@ -1,19 +1,9 @@
+from collections import Counter
 with open("sample.txt", "r", encoding="utf-8") as f:
     text = f.read()
-def count_words(text):
-    words_counts={}
-    low_text=text.lower().split()
-    for char in low_text:
-        if char not in words_counts:
-            words_counts[char]=1
-        else:
-            words_counts[char]+=1
-    text_tupeling=words_counts.items()
-    return sorted(text_tupeling, key=lambda x: x[1],reverse=True)
-
-def print_top(sorted_tuples):
-    for elem in sorted_tuples[0:5]:
-        word,count=elem
+words_counts=Counter(text.split()).most_common(5)
+def print_top(tuples):
+    for element in tuples:
+        word,count=element
         print(f"{word} встретилось {count} раз")
-
-print_top(count_words(text))
+print_top(words_counts)
