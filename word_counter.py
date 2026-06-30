43d78fc2aec6c11fc9f363fb510a700fbe2a2b4b
Bogdan Pavlenko <bogdan2008pavlenko@gmail.com>
2026-06-30 23:19:31 +0300

    add text from file

--- word_counter.py
+++ word_counter.py
@@ -1,15 +1,19 @@
-text = "the quick brown fox the lazy dog the fox runs the dog sleeps the quick fox"
-a={}
-text=text.lower().split(" ")
-for i in text:
-    if i not in a:
-         a[i]=1
-    elif i in a:
-        a[i]+=1
+with open("sample.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+def count_words(text):
+    words_counts={}
+    low_text=text.lower().split(" ")
+    for char in low_text:
+        if char not in words_counts:
+            words_counts[char]=1
+        else:
+            words_counts[char]+=1
+    text_tupeling=words_counts.items()
+    return sorted(text_tupeling, key=lambda x: x[1],reverse=True)
 
-keys=[]
-a=a.items()
-a = sorted(a, key=lambda x: x[1],reverse=True)
-for i in a[0:5]:
-    a,b=i
-    print(f"{a} встретилось {b} раз")
+def print_top(sorted_tuples):
+    for elem in sorted_tuples[0:5]:
+        word,count=elem
+        print(f"{word} встретилось {count} раз")
+
+print_top(count_words(text))
